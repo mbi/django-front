22d67da7e6f8836154533fd9945b561e54c91abb
Marco Bonetti <mbonetti@gmail.com>
2013-05-14 16:51:22 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='django-front',
-    version='0.1.1',
+    version='0.1.2',
     description='A Django application to allow front-end editing',
     author='Marco Bonetti',
     author_email='mbonetti@gmail.com',
