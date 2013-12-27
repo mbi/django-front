47c7c6b6ed85fdd119f5193477bd6b4391521911
Marco Bonetti <mbonetti@gmail.com>
2013-12-27 18:30:36 +0100

    redactor fixed

--- front/static/front/js/front-edit.redactor.js
+++ front/static/front/js/front-edit.redactor.js
@@ -1,17 +1,26 @@
 var front_edit_plugin = {
 
     target: null,
+    initialized: false,
 
     get_container_html: function(element_id, front_edit_options) {
         return '<textarea class="front-edit-container" id="edit-'+ element_id +'"></textarea>';
     },
 
-    set_html: function(target, html, front_edit_options) {
+    __init_editor: function(target, html, front_edit_options) {
         this.target = target;
-
         this.target.addClass('front-edit-redactor');
         var editor_options = $.extend({minHeight:400}, front_edit_options.editor_options);
         this.target.find('.front-edit-container').html(html).redactor(editor_options);
+        this.initialized = true;
+    },
+
+    set_html: function(target, html, front_edit_options) {
+        try {
+            this.target.find('.front-edit-container').redactor('set', html);
+        } catch(err) {
+            this.__init_editor(target, html, front_edit_options);
+        }
     },
 
     get_html: function(front_edit_options) {
