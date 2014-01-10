b96655e992c2f4079aa96fd2cde90ef4c4452d34
Marco Bonetti <mbonetti@gmail.com>
2014-01-10 13:58:04 +0100

    clenaup and pep8 tests

--- front/tests/urls.py
+++ front/tests/urls.py
@@ -3,7 +3,8 @@ try:
 except ImportError:
     from django.conf.urls.defaults import url, patterns, include
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^test/$', 'front.tests.views.test', name='front-test'),
     url(r'^test-invalid/$', 'front.tests.views.test_invalid_template_tag', name='front-test_invalid_template_tag'),
     url(r'^i18n/', include('django.conf.urls.i18n')),
--- runtests_multi_venv.sh
+++ runtests_multi_venv.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
+
 if [ ! -d .venv_14 ]
 then
     virtualenv --no-site-packages --distribute --python=python2 .venv_14
     . .venv_14/bin/activate
-    pip install Django==1.4 coverage python-memcached django-classy-tags south django-wymeditor django-sekizai six
+    pip install Django==1.4 coverage python-memcached django-classy-tags south django-wymeditor django-sekizai six flake8
     deactivate
 fi
 if [ ! -d .venv_15 ]
 then
     virtualenv --no-site-packages --distribute --python=python2 .venv_15
     . .venv_15/bin/activate
-    pip install Django==1.5 coverage python-memcached django-classy-tags south django-wymeditor django-sekizai six
+    pip install Django==1.5 coverage python-memcached django-classy-tags south django-wymeditor django-sekizai six flake8
     deactivate
 fi
 if [ ! -d .venv_15_p3 ]
 then
     virtualenv --no-site-packages --distribute --python=python3 .venv_15_p3
     . .venv_15_p3/bin/activate
-    pip install Django==1.5 coverage python3-memcached django-classy-tags south django-wymeditor django-sekizai six
+    pip install Django==1.5 coverage python3-memcached django-classy-tags south django-wymeditor django-sekizai six flake8
     deactivate
 fi
 if [ ! -d .venv_16 ]
 then
     virtualenv --no-site-packages --distribute --python=python2 .venv_16
     . .venv_16/bin/activate
-    pip install Django==1.6.1 python-memcached six django-classy-tags south django-wymeditor django-sekizai
+    pip install Django==1.6.1 python-memcached six flake8 django-classy-tags south django-wymeditor django-sekizai
     deactivate
 fi
 if [ ! -d .venv_16_p3 ]
 then
     virtualenv --no-site-packages --distribute --python=python3 .venv_16_p3
     . .venv_16_p3/bin/activate
-    pip install Django==1.6.1 python3-memcached six django-classy-tags south django-wymeditor django-sekizai
+    pip install Django==1.6.1 python3-memcached six flake8 django-classy-tags south django-wymeditor django-sekizai
     deactivate
 fi
 
 
-
 . .venv_14/bin/activate
 cd test_project
+flake8 --ignore=E501 --exclude=migrations ../front
 python manage.py --version
 python manage.py test front
 cd ..
@@ -48,6 +49,7 @@ deactivate
 . .venv_15/bin/activate
 cd test_project
 python manage.py --version
+flake8 --ignore=E501 --exclude=migrations ../front
 coverage run --rcfile=.coveragerc manage.py test --failfast front
 coverage xml
 coverage html
@@ -56,14 +58,16 @@ deactivate
 
 . .venv_15_p3/bin/activate
 cd test_project
+flake8 --ignore=E501 --exclude=migrations ../front
 python manage.py --version
 python --version
-python manage.py test front
+ python manage.py test front
 cd ..
 deactivate
 
 . .venv_16/bin/activate
 cd test_project
+flake8 --ignore=E501 --exclude=migrations ../front
 python manage.py --version
 python manage.py test front
 cd ..
@@ -71,6 +75,7 @@ deactivate
 
 . .venv_16_p3/bin/activate
 cd test_project
+flake8 --ignore=E501 --exclude=migrations ../front
 python manage.py --version
 python --version
 python manage.py test front
