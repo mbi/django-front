8a04a88d42b37f4882ab43415e5f20bedae9d257
Leonardo Lazzaro <leonardol@infobytesec.com>
2020-01-02 22:20:40 -0300

    make code compatible with old django version
    Requirement has >=1.8 and tests are being done with django 1.11

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -1,15 +1,14 @@
 # from django.conf.urls import include, url
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path, include
 
 from test_project import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
-    path('ace', views.ace, name='ace'),
-    path('epiceditor', views.epic, name='epic'),
-    path('WYMeditor', views.WYMeditor, name='WYMeditor'),
+    url('', views.home, name='index'),
+    url('ace', views.ace, name='ace'),
+    url('epiceditor', views.epic, name='epic'),
+    url('WYMeditor', views.WYMeditor, name='WYMeditor'),
     url(r'^front-edit/', include('front.urls')),
 ]
 
